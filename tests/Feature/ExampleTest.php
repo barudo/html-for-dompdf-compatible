7243b26da913075bc351ed4250a41dbfdb79a05b
Justin Villocido <barudo@gmail.com>
2026-04-26 23:52:24 +0800

    phpunit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,18 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_homepage_redirects_to_awb_preview(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect('/awb/preview');
+    }
+
+    public function test_awb_preview_returns_a_successful_response(): void
+    {
+        $response = $this->get('/awb/preview');
+
+        $response->assertOk();
+        $response->assertSee('HOUSE AIR WAYBILL');
     }
 }
